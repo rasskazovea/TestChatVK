8e68618e6ecc8f43f1214a0081a8a60655b3f99a
Evgeny Rasskazov <52320302+rasskazovea@users.noreply.github.com>
2020-10-24 14:33:31 +0700

    1

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('server is up and running')
+    res.send('Сервер для чата VK Mini Apps запущен')
 })
 
 module.exports = router
